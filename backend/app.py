51f852c3a2c5c38fcc2e18298b2ed91e955164fe
Gabriel Conte <conte1.gc@gmail.com>
2024-10-18 13:50:32 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from routes import user_routes, event_routes, article_routes, forum_routes
+from routes import user_routes, event_routes, article_routes, forum_routes, admin_routes
 
 
 app = Flask(__name__)
@@ -10,6 +10,7 @@ app.register_blueprint(user_routes.user_routes, url_prefix="/users")
 app.register_blueprint(event_routes.event_routes, url_prefix="/events")
 app.register_blueprint(article_routes.article_routes, url_prefix="/articles")
 app.register_blueprint(forum_routes.forum_routes, url_prefix="/forum")
+app.register_blueprint(admin_routes.admin_routes, url_prefix="/admin")
 
 @app.route("/")
 def home():
